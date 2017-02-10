c6207a0b1803601336ffc636238e397f12a63820
soong0608 <soong0608@stunitas.com>
2017-02-10 13:50:06 +0900

    Login API 완료   ( PASSWORD 암호화 아직 않함 )

--- controller/login.go
+++ controller/login.go
@@ -16,6 +16,7 @@ import (
 	"log"
 	"fmt"
 	_ "go_api/model"
+	usr_err "go_api/error"
 )
 
 func LoginCheck(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +38,9 @@ func LoginCheck(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var request_param vo.LoginApiRequestVo
+	var test string
+	json.Unmarshal(body, test)
+	fmt.Println(test)
 	if err := json.Unmarshal(body, &request_param); err != nil {
 		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(422) // unprocessable entity
@@ -44,28 +48,36 @@ func LoginCheck(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 	}
-	fmt.Println(request_param.User_id)
-	fmt.Println(request_param.Password)
+
+	var password string
 	var user_nm string
-	err = db.QueryRow("SELECT user_nm FROM MEMBER_COMMON WHERE user_id = ?", request_param.User_id).Scan(&user_nm)
+	err = db.QueryRow("SELECT password, user_nm FROM MEMBER_COMMON WHERE user_id = ?", request_param.User_id).Scan(&password, &user_nm)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(user_nm)
-
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(w).Encode(request_param); err != nil {
-		panic(err)
+
+	if password != request_param.Password {
+		var jsonErr = usr_err.HttpError{Code: http.StatusUnauthorized, Text: "Password Not Match"}
+		if err := json.NewEncoder(w).Encode(jsonErr); err != nil {
+			panic(err)
+		}
+	} else {
+		w.WriteHeader(http.StatusCreated)
+
+		json_temp := map[string]interface{}{
+			"code"	: 200,
+			"msg"	: "SUCCESS",
+			"result": map[string]string{
+				"user_nm"	:	user_nm,
+			},
+		}
+
+
+		if err := json.NewEncoder(w).Encode(json_temp); err != nil {
+			panic(err)
+		}
 	}
-
-
-
-	/*fmt.Print("[BODY] =>")
-	fmt.Println(r)*//*
-	result := vo.LoginApiRequestVo{"dofta11","zjaaod11"}
-	json.NewEncoder(w).Encode(result)*/
-
 
 }
--- router/router.go
+++ router/router.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"fmt"
 	"go_api/logger"
 )
 
@@ -12,34 +11,10 @@ func NewRouter() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 	for _, route := range routes {
-
-		fmt.Print(" 1 => ")
-		fmt.Print(routes)
-		fmt.Println()
-		fmt.Print(" 2 => ")
-		fmt.Print(route)
-		fmt.Println()
-
 		var handler http.Handler
 
 		handler = route.HandlerFunc
-		fmt.Print(" 3 => ")
-		fmt.Print(handler)
-		fmt.Println()
 		handler = logger.Logger(handler, route.Name)
-		fmt.Print(" 4 => ")
-		fmt.Print(handler)
-		fmt.Println()
-
-		fmt.Print(" Method => ")
-		fmt.Print(route.Method)
-		fmt.Println()
-		fmt.Print(" Pattern => ")
-		fmt.Print(route.Pattern)
-		fmt.Println()
-		fmt.Print(" Name => ")
-		fmt.Print(route.Name)
-		fmt.Println()
 
 		router.
 			Methods(route.Method).
