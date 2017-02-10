8d7cf8df07e1b6e719dd9d8017b4647f27a0aca6
soong0608 <soong0608@stunitas.com>
2017-02-10 19:03:57 +0900

    todo.go VO

--- controller/web/dice.go
+++ controller/web/dice.go
@@ -3,12 +3,19 @@ package web
 import (
 	"net/http"
 	"fmt"
+	"html/template"
 )
 
 var pr = fmt.Println
 
 func DicePlay(w http.ResponseWriter, r *http.Request){
+
+
 	pr("Dice Play!")
+
+	indexPage, _ := template.New("webpage").Parse("view/dice/main.html")
+	indexPage.Execute(w, nil)
+
 }
 
 
